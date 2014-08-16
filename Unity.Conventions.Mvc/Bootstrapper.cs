6fb5d87b0df82c69292625505d5e883921594b54
José Antonio Plá Rodríguez <jpla83@gmail.com>
2014-08-16 00:53:26 -0500

    first version

---
+++ Unity.Conventions.Mvc/Bootstrapper.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+using Microsoft.Practices.Unity;
+
+namespace Unity.Conventions.Mvc
+{
+    public static class Bootstrapper
+    {
+        public static IUnityContainer Initialise()
+        {
+            var container = BuildUnityContainer();
+            ControllerBuilder.Current.SetControllerFactory(new UnityControllerFactory(container));
+
+            return container;
+        }
+
+        private static IUnityContainer BuildUnityContainer()
+        {
+            var container = IoCManager.Container;
+
+            RegisterTypes(container);
+            return container;
+        }
+
+        private static void RegisterTypes(IUnityContainer container)
+        {
+            // Register here your custom types
+        }
+    }
+}
---
+++ Unity.Conventions.Mvc/UnityControllerFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web.Mvc;
+using Microsoft.Practices.Unity;
+
+namespace Unity.Conventions.Mvc
+{
+    public class UnityControllerFactory : DefaultControllerFactory
+    {
+        private static IUnityContainer _container;
+
+        public UnityControllerFactory(IUnityContainer container)
+        {
+            _container = container;
+        }
+
+        protected override IController GetControllerInstance(System.Web.Routing.RequestContext requestContext, Type controllerType)
+        {
+            return _container.Resolve(controllerType) as IController;
+        }
+    }
+}
